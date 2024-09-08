248d9a7eae6b9edd05a622ca0864fed0e0fa6a18
reha <74599574+rreha@users.noreply.github.com>
2024-09-08 16:43:20 +0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
-var pixel = 0
 $(window).on('scroll', function () {
-    pixel = $(document).scrollTop()/50;
-    $(".bg").css({"-webkit-filter": "blur("+pixel+"px)","-moz-filter": "blur("+pixel+"px)","filter": "blur("+pixel+"px)"})     
+var pixel = Math.min($(document).scrollTop() / 50, 7);
+$(".bg").css({
+    "-webkit-filter": "blur(" + pixel + "px)",
+    "-moz-filter": "blur(" + pixel + "px)",
+    "filter": "blur(" + pixel + "px)"});
 });
+
 function copyBanner() {
     if (navigator.clipboard) {
         navigator.clipboard.writeText('<a href="https://reha.foo.ng"><img src="https://reha.foo.ng/assets/images/reha.gif" border=0></a>')
